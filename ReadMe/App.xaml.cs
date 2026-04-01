ef2ed0061acd5cdaf687cbe4c4b845b23075b1ac
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-04-01 15:42:07 +0200

    feat(VS) : création du projet VS
    [2][DONE]

---
+++ ReadMe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReadMe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
