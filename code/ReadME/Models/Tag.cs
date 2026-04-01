fb841d6b9e4e1ffe23f06a7b4d1d1500ebe49619
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-04-01 15:39:24 +0200

    feat(reset) : suppression de tout pour recommencer car trop de bug
    [38][DONE]
    
    il y a vais trop de bug (environ 86) du coup j'ai décider de recommencer le projet

--- code/ReadME/Models/Tag.cs
+++
@@ -1,9 +0,0 @@
-namespace ReadMe_perso.Models
-{
-    public class Tag
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Color { get; set; } = "#512BD4";
-    }
-}
