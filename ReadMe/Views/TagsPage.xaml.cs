f2378443b7801bbb78b36422f8ccfbc8310f0f64
Gianmarco Ruberti <py13koy@eduvaud.ch>
2026-04-22 16:11:37 +0200

    feat(page) : création du c# de la page de tag
    [8][DONE]

--- ReadMe/Views/TagsPage.xaml.cs
+++ ReadMe/Views/TagsPage.xaml.cs
@@ -1,13 +1,32 @@
 using System;
 using Microsoft.Maui.Controls;
+using ReadMe.Models;
+using ReadMe.ViewModels;
 
 namespace ReadMe.Views
 {
     public partial class TagsPage : ContentPage
     {
+        private TagViewModel _viewModel;
+
         public TagsPage()
         {
             InitializeComponent();
+            _viewModel = new TagViewModel();
+            BindingContext = _viewModel;
+        }
+
+        private void OnSearchButtonPressed(object sender, EventArgs e)
+        {
+            // Logique de recherche de tags
+        }
+
+        private async void OnEditTagClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is Tag tag)
+            {
+                await Shell.Current.GoToAsync($"edittag?tagId={tag.Id}");
+            }
         }
     }
 }
