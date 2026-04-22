ea1c8c424ee37597c3b0165718669ca8176e74a7
Gianmarco Ruberti <py13koy@eduvaud.ch>
2026-04-22 15:40:49 +0200

    feat(page) création de la page de paramètre
    [16][DONE]

---
+++ ReadMe/Views/SettingsPage.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace ReadMe.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            //InitializeComponent();
+        }
+    }
+}
