77bddb6526b65f70041e12cf3f474a251cf52023
Gianmarco Ruberti <py13koy@eduvaud.ch>
2026-04-22 15:13:48 +0200

    feat(page) création de la page Home
    [13][DONE]

--- ReadMe/Views/HomePage.xaml.cs
+++ ReadMe/Views/HomePage.xaml.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.Maui.Controls;
+using ReadMe.Models;
+using ReadMe.ViewModels;
 
 namespace ReadMe.Views
 {
@@ -8,6 +10,14 @@ namespace ReadMe.Views
         public HomePage()
         {
             InitializeComponent();
+            BindingContext = new BookViewModel();
+        }
+        private async void OnReadClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is Book book)
+            {
+                await Shell.Current.GoToAsync($"reader?bookId={book.Id}");
+            }
         }
     }
 }
