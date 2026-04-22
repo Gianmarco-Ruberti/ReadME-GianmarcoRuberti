29bca1822fda03a2b869fe23da2b62cce6aee551
Gianmarco Ruberti <py13koy@eduvaud.ch>
2026-04-22 15:11:34 +0200

    feat(Navigation) mise d'une navigation et de la page home au début
    [11][DONE]

--- ReadMe/MauiProgram.cs
+++ ReadMe/MauiProgram.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
+using ReadMe.Views;
+using ReadMe.ViewModels;
+using ReadMe.Services;
 
 namespace ReadMe
 {
     public static class MauiProgram
     {
+        private static MauiApp _mauiApp;
+
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -15,11 +20,28 @@ namespace ReadMe
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            // Enregistrement des services
+            var appDataDirectory = FileSystem.AppDataDirectory;
+            builder.Services.AddSingleton(new BookService(appDataDirectory));
+            builder.Services.AddSingleton(new TagService(appDataDirectory));
+
+            // Enregistrement des pages et ViewModels
+            builder.Services.AddSingleton<HomePage>();
+            builder.Services.AddSingleton<BookDetailPage>();
+            builder.Services.AddSingleton<ReaderPage>();
+            builder.Services.AddSingleton<TagsPage>();
+
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
+            _mauiApp = builder.Build();
+            return _mauiApp;
+        }
+
+        public static T GetService<T>() where T : class
+        {
+            return _mauiApp.Services.GetService(typeof(T)) as T;
         }
     }
 }
