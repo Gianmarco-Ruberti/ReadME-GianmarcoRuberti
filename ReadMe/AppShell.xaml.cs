fb27dcdc95d0187384bddc88e1da095bd1e2098b
Gianmarco Ruberti <py13koy@eduvaud.ch>
2026-05-20 16:10:30 +0200

    fix(route) ajout des 2 dernière route
    [4][DONE]

--- ReadMe/AppShell.xaml.cs
+++ ReadMe/AppShell.xaml.cs
@@ -10,6 +10,8 @@
             Routing.RegisterRoute("tagsPage", typeof(Views.TagsPage));
             Routing.RegisterRoute("settingsPage", typeof(Views.SettingsPage));
             Routing.RegisterRoute("readerPage", typeof(Views.ReaderPage));
+            Routing.RegisterRoute("EditTagPage", typeof (Views.EditTagPage));
+            Routing.RegisterRoute("BookDetailPage", typeof(Views.BookDetailPage));
         }
 
         private async void OnAccueilClicked(object sender, TappedEventArgs e)
