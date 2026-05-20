4b0de8a99b3aa4b75c285ae3cc67ddfa889a8c5f
Gianmarco Ruberti <py13koy@eduvaud.ch>
2026-05-20 15:59:51 +0200

    fix(Route): correction des route pour la navigation
    [27][DONE]
    
    la majorité du temps est passer sur la DOC

--- ReadMe/AppShell.xaml.cs
+++ ReadMe/AppShell.xaml.cs
@@ -5,26 +5,31 @@
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("homePage", typeof(Views.HomePage));
+            Routing.RegisterRoute("tagsPage", typeof(Views.TagsPage));
+            Routing.RegisterRoute("settingsPage", typeof(Views.SettingsPage));
+            Routing.RegisterRoute("readerPage", typeof(Views.ReaderPage));
         }
 
         private async void OnAccueilClicked(object sender, TappedEventArgs e)
         {
-            await Shell.Current.GoToAsync("home");
+            await Shell.Current.GoToAsync("homePage");
         }
 
         private async void OnBooksClicked(object sender, TappedEventArgs e)
         {
-            await Shell.Current.GoToAsync("home");
+            await Shell.Current.GoToAsync("homePage");
         }
 
         private async void OnTagsClicked(object sender, TappedEventArgs e)
         {
-            await Shell.Current.GoToAsync("tags");
+            await Shell.Current.GoToAsync("tagsPage");
         }
 
         private async void OnSettingsClicked(object sender, TappedEventArgs e)
         {
-            await Shell.Current.GoToAsync("settings");
+            await Shell.Current.GoToAsync("settingsPage");
         }
     }
 }
