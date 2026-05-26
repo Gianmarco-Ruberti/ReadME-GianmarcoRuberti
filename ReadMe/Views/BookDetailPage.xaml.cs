cf71b58e23ed96fb06ee3d9e0a56c4a6b442e33b
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-05-26 19:12:16 +0200

    feat(Views): ajout BookDetailPage pour book details et tag management
    [14][DONE]

--- ReadMe/Views/BookDetailPage.xaml.cs
+++ ReadMe/Views/BookDetailPage.xaml.cs
@@ -1,15 +1,58 @@
 using System;
 using Microsoft.Maui.Controls;
+using ReadMe.Models;
 using ReadMe.ViewModels;
 
 namespace ReadMe.Views
 {
+    [QueryProperty(nameof(BookId), "bookId")]
     public partial class BookDetailPage : ContentPage
     {
+        private readonly BookViewModel _viewModel;
+        private int _bookId;
+
+        public int BookId
+        {
+            get => _bookId;
+            set
+            {
+                _bookId = value;
+                if (_bookId > 0)
+                {
+                    _viewModel.LoadBook(_bookId);
+                }
+            }
+        }
+
         public BookDetailPage()
         {
             InitializeComponent();
-            BindingContext = new BookViewModel();
+            _viewModel = new BookViewModel();
+            BindingContext = _viewModel;
+        }
+
+        private void OnAvailableTagTapped(object sender, EventArgs e)
+        {
+            if (sender is Frame frame && frame.BindingContext is Tag tag)
+            {
+                _viewModel.AddTagToBook(tag);
+            }
+        }
+
+        private async void OnSaveClicked(object sender, EventArgs e)
+        {
+            _viewModel.SaveCurrentBook();
+            await DisplayAlert("Succès", "Livre enregistré avec succès.", "OK");
+            await Shell.Current.GoToAsync("homePage");
+        }
+
+        private async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Confirmation", "Supprimer ce livre ?", "Oui", "Non"))
+            {
+                _viewModel.DeleteCurrentBook();
+                await Shell.Current.GoToAsync("homePage");
+            }
         }
     }
 }
